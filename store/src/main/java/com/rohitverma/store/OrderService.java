559db26ff0a06f976b85fae1e2ef91cf8d3c86dc
Rohit Verma <rohit.verma272727@gmail.com>
2025-03-25 19:51:36 +0530

    feat: Use setter for dependency injection

--- store/src/main/java/com/rohitverma/store/OrderService.java
+++ store/src/main/java/com/rohitverma/store/OrderService.java
@@ -1,12 +1,16 @@
 package com.rohitverma.store;
 
 public class OrderService {
+
     private PaymentService paymentService;
-    public OrderService(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+//    public OrderService(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
     public void placeOrder ()
     {
         paymentService.processPayment(10);
+    }
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
     }
 }
