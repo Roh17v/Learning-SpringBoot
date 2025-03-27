8d74cbfeb52ef54971230b4a7122b7a67b2284c0
Rohit Verma <rohit.verma272727@gmail.com>
2025-03-27 23:18:10 +0530

    feat: Create a bean and inject dependency in a field

---
+++ myFirstProject/src/main/java/com/rohitverma/myFirstProject/Car.java
@@ -0,0 +1,12 @@
+package com.rohitverma.myFirstProject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car {
+
+    public String start()
+    {
+        return "Something";
+    }
+}
--- myFirstProject/src/main/java/com/rohitverma/myFirstProject/MyClass.java
+++ myFirstProject/src/main/java/com/rohitverma/myFirstProject/MyClass.java
@@ -1,13 +1,17 @@
 package com.rohitverma.myFirstProject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyClass {
 
+    @Autowired
+    public Car car;
+
     @RequestMapping("hello")
     public String sayHello() {
-        return "Hello";
+        return car.start();
     }
 }
